da65071864a7a78de53826080207d0ff2e856408
dmznlin <dmznlin@0c8aef3a-167f-237f-8aa4-22847d4104af>
2010-12-05 06:13:44 +0000

    *.添加纸卡冻结和调价.

--- trunk/Source/Forms/UFrameZhiKaDetail.pas
+++ trunk/Source/Forms/UFrameZhiKaDetail.pas
@@ -8,12 +8,13 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, IniFiles, cxStyles, cxCustomData, cxGraphics, cxFilter,
-  cxData, cxDataStorage, cxEdit, DB, cxDBData, Menus, dxLayoutControl,
-  cxMaskEdit, cxButtonEdit, cxTextEdit, ADODB, cxContainer, cxLabel,
-  UBitmapPanel, cxSplitter, cxGridLevel, cxClasses, cxControls,
-  cxGridCustomView, cxGridCustomTableView, cxGridTableView,
-  cxGridDBTableView, cxGrid, ComCtrls, ToolWin, UFrameNormal;
+  Dialogs, IniFiles, UFrameNormal, cxGraphics, cxControls, cxLookAndFeels,
+  cxLookAndFeelPainters, cxStyles, cxCustomData, cxFilter, cxData,
+  cxDataStorage, cxEdit, DB, cxDBData, cxContainer, Menus, dxLayoutControl,
+  cxMaskEdit, cxButtonEdit, cxTextEdit, ADODB, cxLabel, UBitmapPanel,
+  cxSplitter, cxGridLevel, cxClasses, cxGridCustomView,
+  cxGridCustomTableView, cxGridTableView, cxGridDBTableView, cxGrid,
+  ComCtrls, ToolWin;
 
 type
   TfFrameZhiKaDetail = class(TfFrameNormal)
@@ -34,11 +35,21 @@ type
     N2: TMenuItem;
     cxTextEdit3: TcxTextEdit;
     dxLayout1Item5: TdxLayoutItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    N6: TMenuItem;
+    N7: TMenuItem;
+    N8: TMenuItem;
+    N9: TMenuItem;
+    N10: TMenuItem;
     procedure EditZKPropertiesButtonClick(Sender: TObject;
       AButtonIndex: Integer);
     procedure N1Click(Sender: TObject);
     procedure EditDatePropertiesButtonClick(Sender: TObject;
       AButtonIndex: Integer);
+    procedure N8Click(Sender: TObject);
+    procedure N6Click(Sender: TObject);
   private
     { Private declarations }
   protected
@@ -48,6 +59,12 @@ type
     procedure OnDestroyFrame; override;
     function InitFormDataSQL(const nWhere: string): string; override;
     {*查询SQL*}
+    procedure FreezeZK(const nFreeze: Boolean);
+    //冻结选中提货单
+    procedure SelectedZK(const nList: TStrings);
+    //获取选中纸卡号
+    function GetVal(const nRow: Integer; const nField: string): string;
+    //获取指定字段
   public
     { Public declarations }
     class function FrameID: integer; override;
@@ -57,7 +74,8 @@ implementation
 
 {$R *.dfm}
 uses
-  ULibFun, UMgrControl, USysConst, USysDB, UDataModule, UFormDateFilter;
+  ULibFun, UMgrControl, USysConst, USysDB, UDataModule, UFormDateFilter,
+  UFormBase;
 
 //------------------------------------------------------------------------------
 class function TfFrameZhiKaDetail.FrameID: integer;
@@ -79,9 +97,11 @@ end;
 
 function TfFrameZhiKaDetail.InitFormDataSQL(const nWhere: string): string;
 begin
+  N4.Enabled := BtnAdd.Enabled;
+  N6.Enabled := BtnEdit.Enabled;
   EditDate.Text := Format('%s 至 %s', [Date2Str(FStart), Date2Str(FEnd)]);
 
-  Result := 'Select zk.*,zd.*,S_PY,S_Name,C_PY,C_Name From $ZK zk ' +
+  Result := 'Select zk.*,zd.*,zd.R_ID as D_RID,S_PY,S_Name,C_PY,C_Name From $ZK zk ' +
             ' Left Join $SM sm on sm.S_ID=zk.Z_SaleMan' +
             ' Left Join $Cus cus on cus.C_ID=zk.Z_Custom' +
             ' Left Join $ZD zd on zd.D_ZID=zk.Z_ID ' +
@@ -130,15 +150,155 @@ begin
   if ShowDateFilterForm(FStart, FEnd) then InitFormData(FWhere);
 end;
 
+//------------------------------------------------------------------------------
+//Desc: 获取nRow行nField字段的内容
+function TfFrameZhiKaDetail.GetVal(const nRow: Integer;
+ const nField: string): string;
+var nVal: Variant;
+begin
+  nVal := cxView1.DataController.GetValue(
+            cxView1.Controller.SelectedRows[nRow].RecordIndex,
+            cxView1.GetColumnByFieldName(nField).Index);
+  //xxxxx
+
+  if VarIsNull(nVal) then
+       Result := ''
+  else Result := nVal;
+end;
+
+//Desc: 获取选中纸卡列表
+procedure TfFrameZhiKaDetail.SelectedZK(const nList: TStrings);
+var nStr: string;
+    nIdx: Integer;
+begin
+  nList.Clear;
+  for nIdx:=cxView1.Controller.SelectedRowCount - 1 downto 0 do
+  begin
+    nStr := GetVal(nIdx, 'Z_ID');
+    if (nStr <> '') and (nList.IndexOf(nStr) < 0) then
+      nList.Add(nStr);
+    //xxxxx
+  end;
+end;
+
+//Desc: 冻结当前选中的提货单
+procedure TfFrameZhiKaDetail.FreezeZK(const nFreeze: Boolean);
+var nStr,nY: string;
+    nIdx: Integer;
+    nList: TStrings;
+begin
+  if cxView1.DataController.GetSelectedCount < 1 then
+    Exit;
+  //xxxxx
+
+  nList := TStringList.Create;
+  try
+    SelectedZK(nList);
+    if nList.Count < 1 then Exit;
+
+    if nFreeze then
+         nY := '''Y'''
+    else nY := 'Null';
+
+    FDM.ADOConn.BeginTrans;
+    try
+      for nIdx:=nList.Count - 1 downto 0 do
+      begin
+        nStr := 'Update %s Set Z_Freeze=%s Where Z_ID=''%s''';
+        nStr := Format(nStr, [sTable_ZhiKa, nY, nList[nIdx]]);
+        FDM.ExecuteSQL(nStr);
+      end;
+
+      FDM.ADOConn.CommitTrans;
+      InitFormData(FWhere);
+      ShowMsg('操作成功', sHint);
+    except
+      FDM.ADOConn.RollbackTrans;
+      ShowMsg('操作失败', sError);
+    end;
+  finally
+    nList.Free;
+  end;
+end;
+
 //Desc: 快捷查询
 procedure TfFrameZhiKaDetail.N1Click(Sender: TObject);
 begin
   case TComponent(Sender).Tag of
    10: FWhere := Format('Z_InValid=''$Yes''', [sFlag_Yes]);
    20: FWhere := '1=1';
+   30: FreezeZK(True);
+   40: FreezeZK(False);
   end;
 
   InitFormData(FWhere);
+end;
+
+//Desc: 按品种冻结
+procedure TfFrameZhiKaDetail.N8Click(Sender: TObject);
+var nP: TFormCommandParam;
+begin
+  nP.FCommand := cCmd_ModalResult;
+  CreateBaseFormItem(cFI_FormFreezeZK, '', @nP);
+
+  if (nP.FCommand = cCmd_ModalResult) and (nP.FParamA = mrOk) then
+  begin
+    InitFormData(FWhere);
+  end;
+end;
+
+//Desc: 选中纸卡调价
+procedure TfFrameZhiKaDetail.N6Click(Sender: TObject);
+var nList: TStrings;
+    nIdx,nLen: Integer;
+    nP: TFormCommandParam;
+    nStr,nRID,nZID,nStock,nType: string;
+begin
+  if cxView1.DataController.GetSelectedCount < 1 then
+    Exit;
+  //xxxxx
+
+  nList := TStringList.Create;
+  try
+    nType := GetVal(0, 'D_Type');
+    nStock := GetVal(0, 'D_Stock');
+    nLen := cxView1.DataController.GetSelectedCount - 1;
+
+    for nIdx:=0 to nLen do
+    begin
+      nRID := GetVal(nIdx, 'D_RID');
+      nZID := GetVal(nIdx, 'Z_ID');
+      if (nRID = '') or (nZID = '') then Continue;
+
+      if (GetVal(nIdx, 'D_Type') <> nType) or
+         (GetVal(nIdx, 'D_Stock') <> nStock) then
+      begin
+        nStr := '只有同品种的水泥才能统一调价,记录[ %s ]不符合要求.';
+        nStr := Format(nStr, [nRID]);
+        ShowDlg(nStr, sHint, Handle); Exit;
+      end;
+
+      nStr := Format('%s;%s;%s;%s', [nRID,
+              GetVal(nIdx, 'D_Price'), nZID, nStock]);
+      nList.Add(nStr);
+    end;
+
+    if nList.Count < 1 then
+    begin
+      ShowMsg('选中记录无效', sHint); Exit;
+    end;
+
+    nP.FCommand := cCmd_ModalResult;
+    nP.FParamA := Integer(nList);
+    CreateBaseFormItem(cFI_FormAdjustPrice, '', @nP);
+
+    if (nP.FCommand = cCmd_ModalResult) and (nP.FParamA = mrOk) then
+    begin
+      InitFormData(FWhere);
+    end;
+  finally
+    nList.Free;
+  end;
 end;
 
 initialization
