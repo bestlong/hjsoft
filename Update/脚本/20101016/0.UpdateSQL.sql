f34b6f6b7ea4ef7cc06da7f59a123ffa99e1a22c
dmznlin <dmznlin@0c8aef3a-167f-237f-8aa4-22847d4104af>
2010-12-08 07:25:51 +0000

    *.改为压缩包.

--- Update/脚本/20101016/0.UpdateSQL.sql
+++
@@ -1,3 +0,0 @@
-ALTER Table P_Provider Add P_Saler varChar(32);
-ALTER Table P_ProvideLog Add L_PaiNum varChar(15), L_PaiTime DateTime, L_Money Decimal(15, 5), L_YunFei Decimal(15, 5), L_HSer varChar(32), L_HSDate DateTime, L_JSer varChar(32), L_JSDate DateTime;
---修改供应商,供应记录表
